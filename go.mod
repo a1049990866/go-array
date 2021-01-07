4d4a00b34ce70fff0ff0f7cafe2363a98cca6215
pukun <pukun@offcn.com>
2021-01-07 13:42:01 +0800

    update

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module go-array
+module github.com/a1049990866/go-array
 
 go 1.14
--- test/array.go
+++ test/array.go
@@ -1,13 +1,1 @@
 package main
-
-import (
-	"fmt"
-	"go-array/array"
-)
-
-func main() {
-	arr := array.NewArray([]string{"1", "2", "3", "x", "3"}, array.StringArray).Deduplication(true)
-	fmt.Println(arr.Data)
-	//fmt.Println(arr.ToIntArray())
-	fmt.Println(arr.ToFloat32Map())
-}
