be3239c99fc98ab1e29338650ad50d3d3527f358
pukun <pukun@offcn.com>
2021-01-08 21:16:55 +0800

    dict

--- dict/index.go
+++ dict/index.go
@@ -164,20 +164,3 @@ func (t *Dict) Get(key interface{}, args ...interface{}) interface{} {
 	}
 	return out.Interface()
 }
-
-func A() {
-	d := NewDict(nil)
-	d.Set("1", false, 2, "3", 4, 5)
-	fmt.Println(d.Data, "----------", d.Value.Type())
-	d.Set(2, true, 3, "4", 5)
-	fmt.Println(d.Data, "----------", d.Value.Type())
-	d.Set(3, false, 3, 5)
-	fmt.Println(d.Data, "----------", d.Value.Type())
-	d.Set(2, true, 3, "x")
-	fmt.Println(d.Data, "----------", d.Value.Type())
-	d.Set("2", true, "3", "xx")
-	fmt.Println(d.Data, "----------", d.Value.Type())
-	d.Set("3", true, 3, 4)
-	fmt.Println(d.Data, "----------", d.Value.Type())
-	fmt.Println(d.Get(3, 4, 0), d.Error)
-}
--- test/dict.go
+++ test/dict.go
@@ -1,7 +1,23 @@
 package main
 
-import "github.com/a1049990866/go-array/dict"
+import (
+	"fmt"
+	"github.com/a1049990866/go-array/dict"
+)
 
 func main() {
-	dict.A()
+	d := dict.NewDict(nil)
+	d.Set("1", false, 2, "3", 4, 5)
+	fmt.Println(d.Data, "----------", d.Value.Type())
+	d.Set(2, true, 3, "4", 5)
+	fmt.Println(d.Data, "----------", d.Value.Type())
+	d.Set(3, false, 3, 5)
+	fmt.Println(d.Data, "----------", d.Value.Type())
+	d.Set(2, true, 3, "x")
+	fmt.Println(d.Data, "----------", d.Value.Type())
+	d.Set("2", true, "3", "xx")
+	fmt.Println(d.Data, "----------", d.Value.Type())
+	d.Set("3", true, 3, 4)
+	fmt.Println(d.Data, "----------", d.Value.Type())
+	fmt.Println(d.Get(3, 4, 0), d.Error)
 }
